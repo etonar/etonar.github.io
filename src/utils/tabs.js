ce1a0c711d02bd95b9aeb628f01490952bfe42be
etonar <anonymetonar@gmail.com>
2021-08-03 16:22:43 +0100

    Delete src directory

--- src/utils/tabs.js
+++
@@ -1,7 +0,0 @@
-export const tabs = [
-  { id: 1, title: 'featured' },
-  { id: 2, title: 'web app' },
-  { id: 3, title: 'mobile app' },
-  { id: 4, title: 'design' },
-  { id: 5, title: 'branding' }
-]
